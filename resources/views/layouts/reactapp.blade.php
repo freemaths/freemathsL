57143b59ce26ec5e0aa78f1af1b7a8fbe08d2a3b
Ed Darnell <ed@darnell.org.uk>
2018-03-29 16:05:36 +0100

    meta tags

--- resources/views/layouts/reactapp.blade.php
+++ resources/views/layouts/reactapp.blade.php
@@ -2,9 +2,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+<meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Free Maths - GCSE & A Level. Tests, Books & Pasts Papers. New UK syllabus"/>
+<meta name="keywords" content="maths,GCSE maths,AS maths,A2 maths,P1,SM1,C1,C2,C3,C4,M1,M2,S1,S2"/>
+<meta name="author" content="Ed Darnell"/>
 <title>FreeMaths.uk</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'/>
 <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet"/>
