64f8c291b0bc96d99946d50fa32fa07b4760a8fe
Ed Darnell <ed@darnell.org.uk>
2018-10-10 16:55:15 +0100

    service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13eb73e40d1aee1c74504aa805e2cf1f.js"
+  "/precache-manifest.0f4cdd47e009c6508ab2a1c84f8d0a47.js"
 );
 
 workbox.clientsClaim();
