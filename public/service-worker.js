42df9c7e1de12797833a08954f765db5ec86ef02
Ed Darnell <ed@darnell.org.uk>
2018-10-10 17:40:59 +0100

    offline testing

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f4cdd47e009c6508ab2a1c84f8d0a47.js"
+  "/precache-manifest.3347d18472e55f429981d8475f9c0485.js"
 );
 
 workbox.clientsClaim();
