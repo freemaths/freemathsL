11e4ef18bc1077999a53abcd83020a02fcd1248f
edarnell <edarnell@192.168.10.1>
2018-10-11 13:38:03 +0100

    lz caching

---
+++ public/precache-manifest.0b25d2526785094293d6e105bb0e5e7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e11d8605ec2739c42c7b",
+    "url": "/static/js/main.e11d8605.chunk.js"
+  },
+  {
+    "revision": "378cc96e2f36d23988e1",
+    "url": "/static/js/1.378cc96e.chunk.js"
+  },
+  {
+    "revision": "e11d8605ec2739c42c7b",
+    "url": "/static/css/main.5aacd5cf.chunk.css"
+  },
+  {
+    "revision": "18bfebfc9fb15e20fc6c3398d0c8adad",
+    "url": "/index.html"
+  }
+];
