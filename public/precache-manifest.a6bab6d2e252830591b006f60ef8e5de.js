e2bf30a561c818b04ae599fa7f724b486570fd70
Ed Darnell <ed@darnell.org.uk>
2018-10-16 10:55:41 +0100

    .app build

---
+++ public/precache-manifest.a6bab6d2e252830591b006f60ef8e5de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "613d14fc45f600f6651a",
+    "url": "/static/js/main.613d14fc.chunk.js"
+  },
+  {
+    "revision": "6e888a62f78e543b83dd",
+    "url": "/static/js/1.6e888a62.chunk.js"
+  },
+  {
+    "revision": "613d14fc45f600f6651a",
+    "url": "/static/css/main.1042fdca.chunk.css"
+  },
+  {
+    "revision": "34c78625347895ea7ef7770afd6ec105",
+    "url": "/index.html"
+  }
+];
