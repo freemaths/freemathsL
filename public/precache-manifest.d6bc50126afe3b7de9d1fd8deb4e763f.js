1e0a3f0c1d138866e90b32cd954f9f6523f4d6e8
Ed Darnell <ed@darnell.org.uk>
2018-11-23 10:10:52 +0000

    click

--- public/precache-manifest.d6bc50126afe3b7de9d1fd8deb4e763f.js
+++ public/precache-manifest.d6bc50126afe3b7de9d1fd8deb4e763f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "16b1ce127719ed8017d2",
-    "url": "/static/js/main.16b1ce12.chunk.js"
+    "revision": "867d9cf5a9d29a9f2b34",
+    "url": "/static/js/main.867d9cf5.chunk.js"
   },
   {
     "revision": "14a3d51e599ffcf7d880",
     "url": "/static/js/1.14a3d51e.chunk.js"
   },
   {
-    "revision": "16b1ce127719ed8017d2",
+    "revision": "867d9cf5a9d29a9f2b34",
     "url": "/static/css/main.0da4c00e.chunk.css"
   },
   {
-    "revision": "2cc9364454871649aab09353390168ea",
+    "revision": "2d68e96d1328c33a21e60b9b5273b34d",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ef3b2c7a32da2bf5c6dfc22b1a334d1.js"
+  "/precache-manifest.d6bc50126afe3b7de9d1fd8deb4e763f.js"
 );
 
 workbox.clientsClaim();
